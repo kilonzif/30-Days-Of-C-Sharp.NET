463edcce590ffcf80405638e2d0850d6e8f7a527
Kilonzi <Faith.Kilonzi@kenya-airways.com>
2024-01-24 08:24:30 +0300

    Day 1 Done

---
+++ Day1/HelloWorld.cs
@@ -0,0 +1,35 @@
+using System;
+
+// The namespace makes it easier to organize our application and 
+// avoid name collisions with other code
+namespace Day1
+{
+    // In .NET our program is contained within its own class
+    class HelloWorld
+    {
+        // The main function is the entry point into our app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+
+            // create a string variable and ask the user for some input
+            string name = Console.ReadLine(); //eg John
+
+            //Output: Hey there, John
+            Console.WriteLine("Hey there, " + name);
+        }
+    }
+}
+
+
+/***
+---Creatre 
+
+
+Compiling and running (make sure you are in the project directory)
+
+$ dotnet run
+
+
+***/
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 30-Days-Of-C-Sharp.NET
+
+### My Why- Blog[https://faithkilonzi.com/embarking-on-a-coding-odyssey-chronicles-of-my-obsession-for-mastery-as-a-software-engineer/]
+
+###Resources
+
+https://zerotomastery.io/cheatsheets/csharp-cheat-sheet/
+https://quickref.me/cs.html
+https://www.thecodingguys.net/resources/cs-cheat-sheet.pdf
