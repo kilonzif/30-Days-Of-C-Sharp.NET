927d67f213b97012c891b8050ef9ffc022fa96ea
Kilonzi <Faith.Kilonzi@kenya-airways.com>
2024-01-25 09:58:15 +0300

    Day 2 on Data Types and Variables

--- Day1/HelloWorld.cs
+++ Day1/HelloWorld.cs
@@ -2,7 +2,7 @@ using System;
 
 // The namespace makes it easier to organize our application and 
 // avoid name collisions with other code
-namespace Day1
+namespace HelloWorld
 {
     // In .NET our program is contained within its own class
     class HelloWorld
